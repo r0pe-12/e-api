caed09f314a982371caf3b78b01ec267ad864331
Petar Simonovic <simonovicp12@gmail.com>
2022-06-21 23:40:32 +0200

    created model -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products', \App\Http\Controllers\ProductController::class);
+
+Route::group(['prefix'=>'/products'], function (){
+
+    Route::apiResource('/{product}/reviews', \App\Http\Controllers\ReviewController::class);
+
+});
