3e1bc3df5f0bc450d8801c11ba9e0a0a6d59bb07
Petar Simonovic <simonovicp12@gmail.com>
2022-06-22 00:49:22 +0200

    created relationships

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function product(){
+        # code
+        return $this->belongsTo(Product::class);
+    }
 }
