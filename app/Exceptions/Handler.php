136175cab5d8550d9920a154cf611421daf1f865
Petar Simonovic <simonovicp12@gmail.com>
2022-06-22 16:40:32 +0200

    Handler exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Routing\Router;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +53,42 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if (method_exists($e, 'render') && $response = $e->render($request)) {
+            return Router::toResponse($request, $response);
+        }
+
+        if ($e instanceof Responsable) {
+            return $e->toResponse($request);
+        }
+
+        if ($request->expectsJson()){
+            if ($e instanceof ModelNotFoundException){
+                return response()->json([
+                    'errors'=>'Product Model not Found'
+                ], 404);
+            }
+            if ($e instanceof NotFoundHttpException){
+                return response()->json([
+                    'errors'=>'Incorrect route'
+                ], 404);
+            }
+        }
+
+        $e = $this->prepareException($this->mapException($e));
+
+        if ($response = $this->renderViaCallbacks($request, $e)) {
+            return $response;
+        }
+
+        return match (true) {
+            $e instanceof HttpResponseException => $e->getResponse(),
+            $e instanceof AuthenticationException => $this->unauthenticated($request, $e),
+            $e instanceof ValidationException => $this->convertValidationExceptionToResponse($e, $request),
+            default => $this->renderExceptionResponse($request, $e),
+        };
     }
 }
